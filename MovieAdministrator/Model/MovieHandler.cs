2c7a61f2811b0105ede2410e01e0849330273ec7
Morten <morto1995@hotmail.com>
2019-01-30 11:19:30 +0100

    ...

---
+++ MovieAdministrator/Model/MovieHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace MovieAdministrator.Model
+{
+    class MovieHandler
+    {
+        private List<string> _sortingList;
+
+        public MovieHandler()
+        {
+            SortingList = new List<string>(){"Ratings", "A-Z", "Z-A", "Genre"};
+        }
+
+        public List<string> SortingList
+        {
+            get { return _sortingList; }
+            set { _sortingList = value; }
+        }
+    }
+}
---
+++ MovieAdministrator/ViewModel/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MovieAdministrator.Model;
+
+namespace MovieAdministrator.ViewModel
+{
+    class MainViewModel
+    {
+        private MovieHandler _movieHandler = new MovieHandler();
+
+        public MovieHandler MovieHandler
+        {
+            get { return _movieHandler; }
+            set { _movieHandler = value; }
+        }
+    }
+}
